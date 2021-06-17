baf976c4a36097b259ec07b77a686edbb3e35949
andres <velasco.andrs@gmail.com>
2021-06-17 16:26:08 -0500

    alerta login incorrecto

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,6 +1,8 @@
+import { Dispatch } from 'redux';
+import Swal from 'sweetalert2';
+
 import { AuthTypes } from './../types/auth.types';
 import { IUser } from './../interfaces/user.interface';
-import { Dispatch } from 'redux';
 import { AuthService } from '../services/authService';
 
 export const startLogin = (email: string, password: string) => {
@@ -9,11 +11,15 @@ export const startLogin = (email: string, password: string) => {
 
     return async (dispatch: Dispatch<any>) => {
 
-        const user = await AuthService.instance.login(email, password);
+        try {
+            const user = await AuthService.instance.login(email, password);
+            dispatch(login(user));
+        } catch (error) {
+            console.log(error);
+            Swal.fire('Error', 'Credenciales Incorrectas');
+        }
 
-        console.log(user);
 
-        dispatch(login(user));
 
     };
 
